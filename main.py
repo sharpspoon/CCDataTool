9c5f67907552ccba76cfa74dcd535f8a4d7c09af
robin ward <robin@steelcitysites.net>
2018-01-03 16:35:49 -0600

    a

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import csv
-import _random
+import random
+import glob
+
 
 
 fileLoc = raw_input("Please enter the location of the file: ")
 fileName = raw_input("Please enter the name of the file: ")
 
 def readMyFile(filename):
-	
-    dates = []
-    scores = []
+    fullFilePath = fileLoc + fileName
+    if '.csv' in fileName:
+        dates = []
+        scores = []
+        row = 0
 
-    with open(filename) as csvDataFile:
-        csvReader = csv.reader(csvDataFile)
-        for row in csvReader:
-            dates.append(row[0])
-            scores.append(row[1])
+        with open(filename) as csvDataFile:
+            csvReader = csv.reader(csvDataFile)
+            for row in csvReader:
+                dates.append(row[0])
+                scores.append(row[1])
+            for col in row:
+                print 1
+        print 'Opening file...', fullFilePath
 
-    return dates, scores
+        return dates, scores
+    else:
+        dates = 'Error code: '
+        scores = 'only accept .csv at this time'
+        return dates, scores
 
 
 
-fulleFilePath = fileLoc+fileName
-print 'Opening file...',fulleFilePath
 
-dates, scores = readMyFile(fulleFilePath)
+#fulleFilePath = fileLoc+fileName
+fullFilePath = 'c:\est.csv'
+
+
+print 'Opening file...',fullFilePath
+
+dates, scores = readMyFile(fullFilePath)
 
 print(dates)
 print(scores)
