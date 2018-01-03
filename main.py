00d1bad53710883a306bea14f3643f5ea59c2150
robin ward <robin@steelcitysites.net>
2018-01-03 13:14:16 -0600

    commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import csv
+import _random
+def readMyFile(filename):
+    dates = []
+    scores = []
+
+    with open(filename) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        for row in csvReader:
+            dates.append(row[0])
+            scores.append(row[1])
+            print csvDataFile
+
+    return dates, scores
+
+
+dates, scores = readMyFile('c:\est.csv')
+text = raw_input("prompt")
+print text
+print(dates)
+print(scores)
+
