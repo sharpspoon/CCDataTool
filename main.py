8469772d280a9bf062904abf3c7e0561d2a4d081
robin ward <robin@steelcitysites.net>
2018-01-04 09:39:48 -0600

    commented code is work in progress

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
+#import csv
+#import random
+#import glob
+
+
+#print 'CallidusCloud Data Import Tool Copyright 2018'
+#print ''
+#fileLoc = raw_input("Please enter the location of the file: ")
+#fileName = raw_input("Please enter the name of the file: ")
+
+#def readMyFile(fileName):
+#    fullFilePath = fileLoc + fileName
+#    if '.csv' in fileName:
+#        csvArray = []
+#        row = 0
+
+#        with open(fileName) as csvDataFile:
+#            csvReader = csv.reader(csvDataFile)
+#            ncol = len(next(csvReader))
+#            nrow = sum(1 for row in csvDataFile)
+#            print 'ncol',ncol
+#            print 'nrow',nrow
+#            for row in csvReader:
+#                csvArray.append(row[nrow])
+#                nrow = nrow-1
+#                #csvArray.append(row[1])
+#        print 'Opening file...', fullFilePath
+
+#        return csvArray
+#    else:
+
+#        return csvArray
+
+
+
+
+#fullFilePath = fileLoc+fileName
+##fullFilePath = 'c:\est.csv'
+
+
+#print 'Opening file...',fullFilePath
+
+#csvArray = readMyFile(fullFilePath)
+
+#print(csvArray)
+
 import csv
 import random
 import glob
@@ -32,8 +78,8 @@ def readMyFile(filename):
 
 
 
-fullFilePath = fileLoc+fileName
-#fullFilePath = 'c:\est.csv'
+#fulleFilePath = fileLoc+fileName
+fullFilePath = 'c:\est.csv'
 
 
 print 'Opening file...',fullFilePath
